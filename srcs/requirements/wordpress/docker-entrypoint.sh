f4009a5f349641e4f844e18ea2bad79cea390fe1
k-stz@thalassa <keistzen@gmail.com>
2022-04-25 21:54:32 +0200

    add named host volumes via bind

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -98,5 +98,6 @@ fi
 
 # hardcoded install wordpress website, to skip installation dialog on startup
 wp core install --title="Meine Seite" --admin_user=${WORDPRESS_DB_USER} --admin_password=${WORDPRESS_DB_PASSWORD} --admin_email=me@example.com --url=localhost --allow-root
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 
 exec "$@"
