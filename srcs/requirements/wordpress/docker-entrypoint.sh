84f248ca9df7a9ad0a0ce4b3d15afee4848d3f57
k-stz@thalassa <keistzen@gmail.com>
2022-04-27 00:02:57 +0200

    add redis object-cache support

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -96,23 +96,26 @@ if [[ "$1" == apache2* ]] || [ "$1" = 'php-fpm' ]; then
 	fi
 fi
 
-# hardcoded install wordpress website, to skip installation dialog on startup
-wp core install --title="Meine Seite" --admin_user=${WORDPRESS_DB_USER} --admin_password=${WORDPRESS_DB_PASSWORD} --admin_email=me@example.com --url=localhost --allow-root
-
 # simple test if if admin_user is the only user, otherwise the command fails and we get an endless container restarting loop
 result=$(wp user list --allow-root | wc -l)
 if [ $result -lt 3 ]; then
 	wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 fi
 
+# hardcoded install wordpress website, to skip installation dialog on startup
+wp core install --title="Meine Seite" --admin_user=${WORDPRESS_DB_USER} --admin_password=${WORDPRESS_DB_PASSWORD} --admin_email=me@example.com --url=localhost --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp redis enable --force --allow-root
 # define('WP_REDIS_PREFIX', md5($host));
-echo <<END
-define('WP_REDIS_HOST', 'redis');
-define('WP_REDIS_PORT', '6379');
-define('WP_REDIS_MAXTTL', '900');
-define('WP_REDIS_KEY_SALT', 'saaalt');
-END
->> /var/www/html/wp-config.php
+# echo <<END
+# define('WP_REDIS_HOST', 'redis');
+# define('WP_REDIS_PORT', '6379');
+# END
+# >> /var/www/html/wp-config.php
+
+# hacky solution: generate overwrite drop-in for redis-object-cache
+wp redis update-dropin --allow-root
+cp /tmp/object-cache.php /var/www/html/wp-content/object-cache.php
 
 exec "$@"
 
